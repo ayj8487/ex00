91bd313026f1ce718eadda5e86a11e4592128706
ayj8487 <ayj8487@naver.com>
2022-12-19 08:06:40 +0900

    log4jdbc 설정 및 로그레벨 변경

--- ex00/src/main/java/org/zerock/mapper/TimeMapper.java
+++ ex00/src/main/java/org/zerock/mapper/TimeMapper.java
@@ -3,8 +3,16 @@ package org.zerock.mapper;
 import org.apache.ibatis.annotations.Select;
 
 public interface TimeMapper {
-
+	//현재 인터페이스가 존재하고 Mapper XML의 namespace 속성값이 같으면 mybatis가 병합처리
+	
+	// 메서드 선언은 Mapper인터페이스,  SQL 처리는 Mapper XML 
+	
 	@Select("SELECT sysdate FROM dual")
-	public String getTime();
+	public String getTime(); 
+
+	// @Select 와 같이 MyBatis의 어노테이션이 존재하지않고, sql 쿼리도 존재하지않음
+	public String getTime2(); //인터페이스로 작성된 mapper와 xml로 작성된 mapper 비교
+	
+	
 
 }
